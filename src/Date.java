615bab24ab3e1d4e6f2dc08689bf5a5eca1c6295
CeceliaCD <cecelia.chollettedickson@rutgers.edu>
2021-02-01 00:13:29 -0500

    having issue with utilizing calendar class. Errors are popping up although I imported. Cannot test my test case(s) in Date.

--- src/Date.java
+++ src/Date.java
@@ -23,50 +23,52 @@ public class Date {
 	
 	//return today’s date
 	public Date() { 
+		//issue with the getInstance method, maybe Calendar import is not being recognized
 		Calendar calendar.getInstance();
 		System.console().writer().println(calendar.getTime());
 		
 	} 
 	
 	//Question: Is a date that is formatted like mm/d/yyyy and m/dd/yyyy invalid?
-	public isValid() { 
-		//int inputyr = this.year;
-		//int inputmnth = this.month;
-		//int daysin = this.day;
-		
+	public void isValid() { 
+		Date cal = new Date();
+		int inputyr = this.year;
+		int inputmnth = this.month;
+		int daysin = this.day;
 		//Don't know if the way I printed will be acceptable for Professor Chang
 		//Calendar class uses Gregorian and Julian calendars in which JAN represents 0 thus I added plus 1 for month numeric values
-		if(yr < 1900 || (yr > calendar.getInstance().get(Calendar.YEAR) || dy > calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-				|| mnth > calendar.getInstance().get(Calendar.MONTH+1))) {
+		if(inputyr < 1900 || (inputyr > cal.getInstance().get(Calendar.YEAR) 
+				|| daysin > cal.getInstance().get(Calendar.DAY_OF_MONTH)
+				|| inputmnth > cal.getInstance().get(Calendar.MONTH+1))) {
 			System.console().writer().println("Invalid Date!");
 		}
 		
-		boolean leapyr = (yr / 4 == 0) ? true : false;
-		leapyr = (yr/100 == 0) ? true : false;
-		leapyr = (yr/400 == 0) ? true : false;
+		boolean leapyr = (inputyr / 4 == 0) ? true : false;
+		leapyr = (inputyr/100 == 0) ? true : false;
+		leapyr = (inputyr/400 == 0) ? true : false;
 		
 		//this was how I kept the days in each month bounded from 1 to whatever the days limit for that month
-		while(mnth == 1 || mnth == 3 || mnth == 5 || mnth == 7 
-				|| mnth == 8 || mnth == 10 || mnth == 12) {
-			if(dy < 1 || dy > 31) {
+		while(inputmnth == 1 || inputmnth == 3 || inputmnth == 5 || inputmnth == 7 
+				|| inputmnth == 8 || inputmnth == 10 || inputmnth == 12) {
+			if(daysin < 1 || daysin > 31) {
 				System.console().writer().println("Invalid Date!");
 			}
 		}
 		
-		while(mnth == 4 || mnth == 6 || mnth == 9 || mnth == 11) {
-			if(dy < 1 || dy > 30) {
+		while(inputmnth == 4 || inputmnth == 6 || inputmnth == 9 || inputmnth == 11) {
+			if(daysin < 1 || daysin > 30) {
 				System.console().writer().println("Invalid Date!");
 			}
 		}
 		
-		while(!leapyr && mnth == 2) {
-			if(dy < 1 || dy > 28) {
+		while(!leapyr && inputmnth == 2) {
+			if(daysin < 1 || daysin > 28) {
 				System.console().writer().println("Invalid Date!");
 			}
 		}
 		
-		while(leapyr && mnth == 2) {
-			if(dy < 1 || dy > 29) {
+		while(leapyr && inputmnth == 2) {
+			if(daysin < 1 || daysin > 29) {
 				System.console().writer().println("Invalid Date!");
 			}
 		}
@@ -74,8 +76,14 @@ public class Date {
 	}
 	
 	//testbed main
+	/*You MUST design the test cases to thoroughly test the isValid() method. 
+	 * You must write a testbed main and implement the test cases. You must follow the instructions
+	 * in the “Test Specification” section of the Software Development Ground Rules.
+	 */ 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Date published = new Date("01/31/2020");
+		published.isValid();
 
 	}
 
