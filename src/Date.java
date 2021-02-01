5c6ad0071b08573603e4c296cabbee264cc505ef
CeceliaCD <cecelia.chollettedickson@rutgers.edu>
2021-01-31 20:46:37 -0500

    Used calendar class in Date. Still need to finish testbed main.

--- src/Date.java
+++ src/Date.java
@@ -11,8 +11,8 @@ public class Date {
 	private int month; 
 	private int day;
 	
-	//Have not utilized calendar class yet
-	//taking mm/dd/yyyy and create a Date object public Date() { } //return today’s date
+	
+	//taking mm/dd/yyyy and create a Date object public Date() { } 
 	public Date(String date) {
 		Integer yr = new Integer(this.year);
 		Integer mnth = new Integer(this.month);
@@ -21,45 +21,52 @@ public class Date {
 		String dateFormat = Integer.toString(mnth) + "/" + Integer.toString(dy) + "/" + Integer.toString(yr);	
 	} 
 	
-	public Date() { } //return today’s date
+	//return today’s date
+	public Date() { 
+		Calendar calendar.getInstance();
+		System.console().writer().println(calendar.getTime());
+		
+	} 
 	
 	//Question: Is a date that is formatted like mm/d/yyyy and m/dd/yyyy invalid?
 	public isValid() { 
-		int inputyr = this.year;
-		int inputmnth = this.month;
-		int daysin = this.day;
+		//int inputyr = this.year;
+		//int inputmnth = this.month;
+		//int daysin = this.day;
 		
 		//Don't know if the way I printed will be acceptable for Professor Chang
-		if(inputyr < 1900) {
+		//Calendar class uses Gregorian and Julian calendars in which JAN represents 0 thus I added plus 1 for month numeric values
+		if(yr < 1900 || (yr > calendar.getInstance().get(Calendar.YEAR) || dy > calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+				|| mnth > calendar.getInstance().get(Calendar.MONTH+1))) {
 			System.console().writer().println("Invalid Date!");
 		}
 		
-		boolean leapyr = (inputyr / 4 == 0) ? true : false;
-		leapyr = (inputyr/100 == 0) ? true : false;
-		leapyr = (inputyr/400 == 0) ? true : false;
+		boolean leapyr = (yr / 4 == 0) ? true : false;
+		leapyr = (yr/100 == 0) ? true : false;
+		leapyr = (yr/400 == 0) ? true : false;
 		
 		//this was how I kept the days in each month bounded from 1 to whatever the days limit for that month
-		while(inputmnth == 1 || inputmnth == 3 || inputmnth == 5 || inputmnth == 7 
-				|| inputmnth == 8 || inputmnth == 10 || inputmnth == 12) {
-			if(daysin < 1 || daysin > 31) {
+		while(mnth == 1 || mnth == 3 || mnth == 5 || mnth == 7 
+				|| mnth == 8 || mnth == 10 || mnth == 12) {
+			if(dy < 1 || dy > 31) {
 				System.console().writer().println("Invalid Date!");
 			}
 		}
 		
-		while(inputmnth == 4 || inputmnth == 6 || inputmnth == 9 || inputmnth == 11) {
-			if(daysin < 1 || daysin > 30) {
+		while(mnth == 4 || mnth == 6 || mnth == 9 || mnth == 11) {
+			if(dy < 1 || dy > 30) {
 				System.console().writer().println("Invalid Date!");
 			}
 		}
 		
-		while(!leapyr && inputmnth == 2) {
-			if(daysin < 1 || daysin > 28) {
+		while(!leapyr && mnth == 2) {
+			if(dy < 1 || dy > 28) {
 				System.console().writer().println("Invalid Date!");
 			}
 		}
 		
-		while(leapyr && inputmnth == 2) {
-			if(daysin < 1 || daysin > 29) {
+		while(leapyr && mnth == 2) {
+			if(dy < 1 || dy > 29) {
 				System.console().writer().println("Invalid Date!");
 			}
 		}
