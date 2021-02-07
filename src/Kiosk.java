b52a1b13dd71d1df3ff27677272eec2d2569248b
ansanida <33883725+ansanida@users.noreply.github.com>
2021-02-07 18:39:01 -0500

    Update Kiosk.java

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -10,63 +10,44 @@ public class Kiosk {
 
 	public void run() { 
 		
-		Scanner input = new Scanner(System.in);
-
-		
+		Scanner input = new Scanner(System.in);	
 	
 	while (true) {
 		
 		String choice = input.nextLine();
 		char firstChar = choice.charAt(0);
-		
-		//f (firstChar == 'W') {
-		String delim = ",";
-		StringTokenizer st = new StringTokenizer(choice, delim);
+
+		//String delim = ",";
+		//StringTokenizer st = new StringTokenizer(choice, delim);
 		String[] split = choice.split(",");
-		Book books;
-		//String name = split[1];
-		String name = split[1].setName();
-		//int i = 0;
-		//String[] line = st.next().toStringArray;
-		//while (st.hasMoreElements()) {		
-		//	line[i] = (String) st.nextElement();
-		//	i++;
-		//}
-		//System.out.println("3rd element: " + split[2]);
-		//}
+		//String name = split[1].setName();
 		
 		// all the if statements have a phrase to output rn, i was just testing!
 		if (firstChar == 'A') {
-			System.out.println("hello!");
+			//addBook();
 		}
 		
 		if (firstChar == 'R') {
-			System.out.println("hello!1");
 			//remove();
 		}
 		
 		if (firstChar == 'O') {
-			System.out.println("hello!2");
 			//checkOut();
 		}
 		
 		if (firstChar == 'I') {
-			System.out.println("hello!3");
 			//returnBook();
 		}
 		
 		if (choice.equals("PA")) {
-			System.out.println("hello!4");
 			//currSequence();
 		}
 		
 		if (choice.equals("PD")) {
-			System.out.println("hello!5");
 			//dateAscOrder();
 		}
 		
 		if (choice.equals("PN")) {
-			System.out.println("hello!6");
 			//bookAscOrder();
 		}
 		if (firstChar == 'Q') {
